6b725c13d541477e07086ef8bc644668cfb48179
irfanalam0123 <irfanalam0123bth@gmail.com>
2025-09-14 18:38:24 +0530

    Update backend build script, setup frontend + backend properly

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    // console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     console.log("MONGO_URI from env:", process.env.MONGO_URI);
   } catch (error) {
     console.error(`❌ MongoDB Error: ${error.message}`);
